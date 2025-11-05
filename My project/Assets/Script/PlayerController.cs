2f15e03a000b3d3174ee6dcd7e8270c2883a84d1
kevinotto <ottokoit@gmail.com>
2025-11-05 12:40:14 +0200

    v.1

---
+++ My project/Assets/Script/PlayerController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float FlySpeed = 5;
+    public float YawAmount = 120;
+    private float Yaw;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move forward
+        transform.position += transform.forward * FlySpeed * Time.deltaTime;
+
+        //inputs
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //yaw, pitch, roll
+        Yaw += horizontalInput * YawAmount * Time.deltaTime;
+        float pitch=Mathf.Lerp(0, 90, Mathf.Abs(verticalInput))*Mathf.Sign(verticalInput);
+        float roll=Mathf.Lerp(0, 20, Mathf.Abs(horizontalInput))*-Mathf.Sign(horizontalInput);
+
+        //apply rotation
+        transform.localRotation = Quaternion.Euler(Vector3.up * Yaw + Vector3.right * pitch + Vector3.forward * roll);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "danger")
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
